8075688bcce1f03054c70d8ca7b346eeb220c12b
hasanhamadi <hasanias1385@gmail.com>
2026-04-05 16:14:20 +0330

    h

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.example.cropino"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+    // اصلاح نسخه NDK برای هماهنگی با پکیج webview
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +22,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cropino"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // استفاده از مقادیر پیش‌فرض فلاتر
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +32,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // تنظیمات امضا برای نسخه ریلیز
             signingConfig = signingConfigs.getByName("debug")
         }
     }
